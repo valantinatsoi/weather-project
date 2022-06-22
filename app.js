2ce8d95e548968c0947ff1b760ffaccb1baddcb4
Valantina Tsoi <99823443+valantinatsoi@users.noreply.github.com>
2022-06-22 17:46:21 +0800

    can check any city

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
+app.use(bodyParser.urlencoded({extended: true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?appid=7db442f1038a38ba42ed8b3eef72d4af&q=Shanghai&units=metric";
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req, res){
+  const query = req.body.cityName;
+  const apiKey = "7db442f1038a38ba42ed8b3eef72d4af"
+  const unit = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + query + "&units=" + unit;
 
   https.get(url, function (response){
     console.log(response.statusCode);
@@ -18,10 +27,11 @@ app.get("/", function(req, res){
       const icon = weatherData.weather[0].icon
       const description = weatherData.weather[0].description
       res.write("<h1>" + city + "</h1>");
-      res.write("<h2>The temperature here is " + temp + " degrees celsius.</h2>");
-      res.write("<p>The humidity is " + humidity + " %.</p>");
+      res.write("<h2>The temperature is " + temp + " degrees celsius.</h2>");
+      res.write("<p>The humidity is " + humidity + "%.</p>");
       res.write("<p>The weather is currently " + description + ".</p>");
       res.write('<img src="http://openweathermap.org/img/wn/' + icon + '@2x.png" alt="weather-icon">')
+      res.write('<button onclick="history.back()">check another city</button>')
       res.send();
     })
   });
